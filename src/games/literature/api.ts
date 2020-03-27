b5016f972c2b1381dc6227ffc084626ce06c7690
Naven Rajkumar <naven1999@gmail.com>
2020-03-27 12:29:38 +0530

    Feat: Change game REST routes to socket channel

--- src/games/literature/api.ts
+++ src/games/literature/api.ts
@@ -5,14 +5,17 @@ import * as Validator from '../../util/validator'
 import { Game } from '../../engine'
 
 let router = express.Router()
-let io,
-	litNsp,
+let litNsp,
 	gameData = {},
 	socketIDMap = {}
 
 const baseUrl = '/literature'
 router.post(baseUrl + '/create', async (req, res) => {
-	let player = await LiteratureController.registerPlayer(req.body.pid, req.body.name, req.body.pos)
+	let player = await LiteratureController.registerPlayer(
+		req.body.pid,
+		req.body.name,
+		req.body.pos
+	)
 	let game = await LiteratureController.hostGame(player)
 	setGameData(game.code, game)
 	res.send({
@@ -24,8 +27,12 @@ router.post(baseUrl + '/create', async (req, res) => {
 router.post(baseUrl + '/join', async (req, res) => {
 	try {
 		let game = getGameData(req.body.gameCode)
-		let player = await LiteratureController.registerPlayer(req.body.pid, req.body.name, req.body.pos)
-		
+		let player = await LiteratureController.registerPlayer(
+			req.body.pid,
+			req.body.name,
+			req.body.pos
+		)
+
 		await LiteratureController.joinGame(game, player)
 		setGameData(game.code, game)
 		res.send({
@@ -38,84 +45,7 @@ router.post(baseUrl + '/join', async (req, res) => {
 	}
 })
 
-router.post(baseUrl + '/start', async (req, res) => {
-	try {
-		let game = await LiteratureController.getGame(req.body.code)
-		let player = game.getPlayerById(req.body.pid)
-
-		Validator.isOwner(game, player)
-		LiteratureController.startGame(game)
-
-		setGameData(game.code, game)
-		res.send({
-			playerId: player.id
-		})
-	} catch (err) {
-		res.send({
-			error: err.message
-		})
-	}
-})
-
-router.post(baseUrl + '/play/ask', async (req, res) => {
-	try {
-		let game = getGameData(req.body.gcode)
-		let from = game.getPlayerById(req.body.fid)
-
-		Validator.isMyTurn(game, from)
-		LiteratureValidator.canAsk(from, req.body.card)
-		let to = game.getPlayerByPosition(req.body.tpos)
-
-		LiteratureController.askForCard(game, from, to, req.body.card)
-		res.send({
-			result: "Asked for card"
-		})
-	} catch (err) {
-		res.send({
-			error: err.message
-		})
-	}
-})
-
-router.post(baseUrl + '/play/transfer', async (req, res) => {
-	try {
-		let game = getGameData(req.body.gcode)
-		let from = game.getPlayerById(req.body.pid)
-
-		Validator.isMyTurn(game, from)
-		let to = game.getPlayerByPosition(req.body.tpos)
-
-		Validator.areSameTeam(from, to)
-		LiteratureController.transferTurn(game, to)
-		res.send({
-			result: "Transfered turn"
-		})
-	} catch (err) {
-		res.send({
-			error: err.message
-		})
-	}
-})
-
-router.post(baseUrl + '/play/declare', async (req, res) => {
-	try {
-		let game = getGameData(req.body.gcode)
-		let from = game.getPlayerById(req.body.pid)
-
-		Validator.isMyTurn(game, from)
-		LiteratureController.declareSet(game, from, req.body.declaration)
-		res.send({
-			result: "Declared set"
-		})
-	} catch (err) {
-		res.send({
-			error: err.message
-		})
-	}
-})
-
 var setupLiteratureGame = (ioObject, litNspObject) => {
-	io = ioObject
 	litNsp = litNspObject
 	openSocketChannels()
 }
@@ -130,7 +60,7 @@ var setGameData = (gameCode: string, game: Game) => {
 
 var openSocketChannels = (): void => {
 	litNsp.on('connection', (socket) => {
-		socket.on('connectGame', (data) => {
+		socket.on('connect-game', (data) => {
 			var playerId = data.playerId
 			var gameCode = data.gameCode
 			var player = getGameData(gameCode).getPlayerById(playerId)
@@ -142,8 +72,93 @@ var openSocketChannels = (): void => {
 				.emit('gameUpdates', player.name + ' has joined the game')
 		})
 
-		socket.on('ask', (data) => {
-			//LiteratureController.askForCard(getGameData(data.gameCode), data.fromPlayerId, data.toPlayerId)
+		socket.on('start-game', (data) => {
+			let gameCode = data.gameCode
+			let playerId = data.playerId
+
+			try {
+				let game = getGameData(gameCode)
+				let player = game.getPlayerById(playerId)
+
+				Validator.isOwner(game, player)
+				LiteratureController.startGame(game)
+
+				setGameData(game.code, game)
+				litNsp.to(gameCode).emit('gameUpdates', 'Started game')
+			} catch (err) {
+				litNsp.to(gameCode).emit('gameUpdates', err.message)
+			}
+		})
+
+		socket.on('play-ask', (data) => {
+			let card = data.card
+			let gameCode = data.gameCode
+
+			try {
+				let game = getGameData(gameCode)
+				let fromPlayer = game.getPlayerByPosition(data.fromId)
+				let toPlayer = game.getPlayerByPosition(data.toPos)
+
+				Validator.isMyTurn(game, fromPlayer)
+				LiteratureValidator.canAsk(fromPlayer, card)
+				LiteratureController.askForCard(
+					game,
+					fromPlayer,
+					toPlayer,
+					card
+				)
+				litNsp
+					.to(gameCode)
+					.emit(
+						'play-ask',
+						fromPlayer.name +
+							' has asked ' +
+							toPlayer.name +
+							' for the card ' +
+							card
+					)
+			} catch (err) {
+				litNsp.to(gameCode).emit('play-ask', err.message)
+			}
+		})
+
+		socket.on('play-declare', (data) => {
+			let gameCode = data.gameCode
+			let playerId = data.playerId
+			let declaration = data.declaration
+
+			try {
+				let game = getGameData(gameCode)
+				let from = game.getPlayerById(playerId)
+
+				Validator.isMyTurn(game, from)
+				LiteratureController.declareSet(game, from, declaration)
+				litNsp
+					.to(gameCode)
+					.emit('play-declare', 'Declared set successfully')
+			} catch (err) {
+				litNsp.to(gameCode).emit('play-declare', err.message)
+			}
+		})
+
+		socket.on('play-transfer', (data) => {
+			let gameCode = data.gameCode
+			let fromId = data.fromId
+			let toPos = data.toPos
+
+			try {
+				let game = getGameData(gameCode)
+				let from = game.getPlayerById(fromId)
+
+				Validator.isMyTurn(game, from)
+				let to = game.getPlayerByPosition(toPos)
+
+				Validator.areSameTeam(from, to)
+				LiteratureController.transferTurn(game, to)
+				litNsp.to(gameCode).emit('play-transfer', 'Transferred turn')
+			} catch (err) {
+				litNsp.to(gameCode).emit('play-transfer', err.message)
+			}
 		})
 	})
 }
